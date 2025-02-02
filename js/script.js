44b14ada064b0f0e868335dda4da6a1895933a90
Dilip Parmar <pilid831@gmail.com>
2025-02-02 18:31:10 +0530

    committing new changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 var app = angular.module('myApp',[]);
 app.controller('myCtrl',function($scope){    
     // Example usage
-    const myList = [0, 1, 2, 3, 4, 5, 6, 7];
+    const n = 4;
+    $scope.elen = 100;
+    let eleLen = 100;    
+    if (n > 4){
+        eleLen = Math.floor(400/n);
+        console.log("Original List:", eleLen);
+        //boxLen = eleLen*n;
+    }
+    $scope.elen = eleLen;
+    var parent = document.getElementById("parent");
+    parent.style.width = n*eleLen + 'px';
+    parent.style.height = n*eleLen + 'px';
+    const grid = n*n - 1;
+    const myList = [];
+    for (let i = 0 ; i < grid ; i++){
+        myList.push(i);
+    }
+    //const myList = [0, 1, 2, 3, 4, 5, 6, 7];
     console.log("Original List:", myList);
     
     const shuffledList = shuffleArray1(myList);
@@ -9,21 +26,21 @@ app.controller('myCtrl',function($scope){
 
     $scope.myobj = [{x:0,y:0,v:1},{x:100,y:100,v:2}];
     const a = [];
-    for(var i = 0; i < 3; i++){
-    for(var j= 0; j <3; j++){
-        if(!(i == 2 && j == 2))
-        a.push({x:100*j,y:100*i,v:3*i+j});
+    for(var i = 0; i < n; i++){
+    for(var j= 0; j <n; j++){
+        if(!(i == (n-1) && j == (n-1)))
+        a.push({x:eleLen*j,y:eleLen*i,v:n*i+j});
     }     
     
     }
     const b = [];
-    for(var p = 0; p<8;p++ ){
+    for(var p = 0; p < grid;p++ ){
     let temVar = a[shuffledList[p]];
     b.push({x:temVar.x,y:temVar.y,v:p});
     }
     $scope.myobj = b;
     $scope.flag = false;
-    $scope.empty = {x:200,y:200};
+    $scope.empty = {x:eleLen*(n-1),y:eleLen*(n-1)};
     $scope.fun = function (i,$event){
     const ele = $event.target;
     let currentTop = parseInt(window.getComputedStyle(ele).top);
@@ -40,9 +57,9 @@ app.controller('myCtrl',function($scope){
         $scope.myobj[i].x = emptyLeft;
     }
     let flag = 1;
-    for(var k = 0; k<8; k++){
-        let curVal = 1*$scope.myobj[k].x + 3*$scope.myobj[k].y;
-        if (curVal == 100*k){
+    for(var k = 0; k< grid; k++){
+        let curVal = 1*$scope.myobj[k].x + n*$scope.myobj[k].y;
+        if (curVal == eleLen*k){
         flag = flag*1;
         }else{
         flag = flag*0;
@@ -50,9 +67,12 @@ app.controller('myCtrl',function($scope){
     }
     if (flag == 1){
         $scope.flag = true;
+        animateWin();
     }
     console.log($scope.empty.x,$scope.empty.y);        
     };
+
+    
     
 });
 
@@ -88,3 +108,13 @@ function shuffleArray1(array){
     }
     return arr;
 }
+
+function animateWin() {
+    gsap.from("#win-text", {
+        opacity: 0,
+        y: -100,
+        duration: 1,
+        ease: "bounce"
+    });
+}    
+
